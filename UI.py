91841ce4232e76de8ad87c03344a75ab99488ac7
firsh <alex.firsh@gmail.com>
2013-07-19 00:47:32 +0300

    1) delete some useless var. 2) ins menu is now specify by dict too, add useless func crt_geom()

--- UI.py
+++ UI.py
@@ -5,6 +5,9 @@ from tkinter import *
 def destroyer():
     root.destroy()
 
+def crt_geom():
+    """Temp func"""
+    pass
 
 class Button_process():
     def __init__(self, workspace, text_var, process):
@@ -29,47 +32,39 @@ class Label_process():
 
 class Menu_process():
     def __init__(self, workspace):
-        self.workspace = workspace
-        self.menu_base = Menu(self.workspace, tearoff=0)
-        self.workspace.config(menu=self.menu_base)
-
+        self.menu_base = Menu(workspace, tearoff=0)
+        workspace.config(menu=self.menu_base)
 
     def menu_col(self, title, inc_dict):
         self.menu_col_1 = Menu(self.menu_base, tearoff=0)
-        self.title = title
-        self.menu_base.add_cascade(label=self.title, menu=self.menu_col_1)
-        '''destroyer(???) '''
+        self.menu_base.add_cascade(label=title, menu=self.menu_col_1)
         for i, j in inc_dict.items():
             if i == "Exit":
                 self.menu_col_1.add_separator()
-            self.menu_col_1.add_command(label=i, command = j)
+            self.menu_col_1.add_command(label=i, command=j)
         return self.menu_col_1
 
-
-    def menu_ins(self, menu_from, index, title, *args):
-        self.menu_from = menu_from
-        self.menu_internal = Menu(self.menu_from, tearoff=0)
-        self.title = title
-        self.index = index
-        self.menu_from.insert_cascade(index, label=title, menu=self.menu_internal)
-        for i in args:
-            self.menu_internal.add_command(label=i)
+    def menu_ins(self, menu_from, index, title, inc_dict):
+        self.menu_internal = Menu(menu_from, tearoff=0)
+        menu_from.insert_cascade(index, label=title, menu=self.menu_internal)
+        for i,j in inc_dict.items():
+            self.menu_internal.add_command(label=i, command = j)
 
 
 class Windows:
     def __init__(self, workspace):
-        self.workspace = workspace
-        self.new_win = Toplevel(self.workspace)
+        self.new_win = Toplevel(workspace)
 
 
 root = Tk()
 
 BaseMenu = Menu_process(root)
 
-under_menu = BaseMenu.menu_col('File', {'Exit':destroyer})
+under_menu = BaseMenu.menu_col('File', {'Exit': destroyer})
+# THINK how pass an argument "workspace" to destroyer
 # under_2_menu = BaseMenu.menu_col('Help')
 
-und_under_menu = BaseMenu.menu_ins(under_menu, 0, 'Model', 'Create rectangle', 'Create Oval')
+und_under_menu = BaseMenu.menu_ins(under_menu, 0, 'Model', {'Create rectangle':crt_geom(), 'Create Oval':crt_geom()})
 # und_under_menu = BaseMenu.menu_ins(under_2_menu, 'Version', 'Check for updates', 'Title')
 
 Present_1 = Label_process(root, 'Женечка уже заебал спать?', 'Arial 20')
