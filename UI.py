965ea4a4c30c728363ffc45ee058770548e66e9b
Alexey Firsh <alexey.firsh@gmail.com>
2013-07-18 08:20:07 +0300

    Update UI.py
    delete useless var

--- UI.py
+++ UI.py
@@ -29,29 +29,22 @@ class Label_process():
 
 class Menu_process():
     def __init__(self, workspace):
-        self.workspace = workspace
-        self.menu_base = Menu(self.workspace, tearoff=0)
-        self.workspace.config(menu=self.menu_base)
-
+        self.menu_base = Menu(workspace, tearoff=0)
+        workspace.config(menu=self.menu_base)
 
     def menu_col(self, title, inc_dict):
         self.menu_col_1 = Menu(self.menu_base, tearoff=0)
-        self.title = title
-        self.menu_base.add_cascade(label=self.title, menu=self.menu_col_1)
+        self.menu_base.add_cascade(label=title, menu=self.menu_col_1)
         '''destroyer(???) '''
         for i, j in inc_dict.items():
             if i == "Exit":
                 self.menu_col_1.add_separator()
-            self.menu_col_1.add_command(label=i, command = j)
+            self.menu_col_1.add_command(label=i, command=j)
         return self.menu_col_1
 
-
     def menu_ins(self, menu_from, index, title, *args):
-        self.menu_from = menu_from
-        self.menu_internal = Menu(self.menu_from, tearoff=0)
-        self.title = title
-        self.index = index
-        self.menu_from.insert_cascade(index, label=title, menu=self.menu_internal)
+        self.menu_internal = Menu(menu_from, tearoff=0)
+        menu_from.insert_cascade(index, label=title, menu=self.menu_internal)
         for i in args:
             self.menu_internal.add_command(label=i)
 
@@ -66,7 +59,7 @@ root = Tk()
 
 BaseMenu = Menu_process(root)
 
-under_menu = BaseMenu.menu_col('File', {'Exit':destroyer})
+under_menu = BaseMenu.menu_col('File', {'Exit': destroyer, })
 # under_2_menu = BaseMenu.menu_col('Help')
 
 und_under_menu = BaseMenu.menu_ins(under_menu, 0, 'Model', 'Create rectangle', 'Create Oval')
