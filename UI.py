21a5b4740b0c1fb70521a5b78acb7bdd0683a9c8
firsh <alex.firsh@gmail.com>
2013-07-22 02:10:56 +0300

    Created table, new grid package for all widgets.
    Table is array of label and listbox, which switch their width and height from max element.

--- UI.py
+++ UI.py
@@ -2,21 +2,26 @@ __author__ = 'Admin'
 from tkinter import *
 
 
-def destroyer():
-    root.destroy()
-
-def crt_geom():
-    """Temp func"""
-    pass
-
-class Button_process():
-    def __init__(self, workspace, text_var, process):
+class ButtonProcess():
+    def __init__(self, workspace, text_var, process, row, col):
         self.but = Button(workspace, text=text_var)
-        self.but.pack()
-        self.but.bind(process, self.func)
+        self.but.grid(row=row, column=col)
+        self.but.bind(process, act.func)
+
+
+class Actions():
+    def __init__(self, workspace, text_var, process):
         self.workspace = workspace
         self.text_var = text_var
         self.process = process
+
+    def crt_geom(self):
+        """Temp func"""
+
+    pass
+
+    def destroyer(self):
+        root.destroy()
 
     def func(self, event):
         self.new_but = Button(self.workspace, text=self.text_var)
@@ -24,13 +29,31 @@ class Button_process():
         self.new_but.bind(self.process, self.func)
 
 
-class Label_process():
-    def __init__(self, workspace, text_var, font_var):
+class LabelProcess():
+    def __init__(self, workspace, text_var, font_var, row, col):
         self.label = Label(workspace, text=text_var, font=font_var)
-        self.label.pack()
+        self.label.grid(row=row, column=col)
 
 
-class Menu_process():
+class ListProcess():
+    def __init__(self, workspace, row, col, name):
+        if len(name) == 1: self.maximum = len(name[0])
+        if len(name) == 0: self.maximum = 2
+        else:
+            first_it = len(name[0])
+            for j in range(len(name) - 1):
+                if first_it >= len(name[j + 1]):
+                    self.maximum = first_it
+                else:
+                    first_it = len(name[j + 1])
+                    self.maximum = first_it
+        self.listbox = Listbox(workspace, height=len(name), width=self.maximum)
+        for i in name:
+            self.listbox.insert(END, i)
+        self.listbox.grid(row=row, column=col)
+
+
+class MenuProcess():
     def __init__(self, workspace):
         self.menu_base = Menu(workspace, tearoff=0)
         workspace.config(menu=self.menu_base)
@@ -47,8 +70,8 @@ class Menu_process():
     def menu_ins(self, menu_from, index, title, inc_dict):
         self.menu_internal = Menu(menu_from, tearoff=0)
         menu_from.insert_cascade(index, label=title, menu=self.menu_internal)
-        for i,j in inc_dict.items():
-            self.menu_internal.add_command(label=i, command = j)
+        for i, j in inc_dict.items():
+            self.menu_internal.add_command(label=i, command=j)
 
 
 class Windows:
@@ -56,20 +79,33 @@ class Windows:
         self.new_win = Toplevel(workspace)
 
 
-root = Tk()
+if __name__ == "__main__":
+    root = Tk()
 
-BaseMenu = Menu_process(root)
+    base_menu = MenuProcess(root)
+    act = Actions(root, 'Newbut', "<Button-1>")
 
-under_menu = BaseMenu.menu_col('File', {'Exit': destroyer})
-# THINK how pass an argument "workspace" to destroyer
-# under_2_menu = BaseMenu.menu_col('Help')
-#хуй вам в рот, гит
-und_under_menu = BaseMenu.menu_ins(under_menu, 0, 'Model', {'Create rectangle':crt_geom(), 'Create Oval':crt_geom()})
-# und_under_menu = BaseMenu.menu_ins(under_2_menu, 'Version', 'Check for updates', 'Title')
+    under_menu = base_menu.menu_col('File', {'Exit': act.destroyer})
+    # THINK how pass an argument "workspace" to destroyer
+    # under_2_menu = BaseMenu.menu_col('Help')
 
-Present_1 = Label_process(root, 'Женечка уже заебал спать?', 'Arial 20')
-Button_1 = Button_process(root, 'Нет, он - няша', "<Button-1>")
-Button_1 = Button_process(root, 'Да, пойду налимоню ему очко!', "<Button-1>")
+    und_under_menu = base_menu.menu_ins(under_menu, 0, 'Model',
+                                        {'Create rectangle': act.crt_geom(), 'Create Oval': act.crt_geom(),
+                                         'Remove': act.crt_geom()})
 
-# root.destroy()
-root.mainloop()
+    # und_under_menu = BaseMenu.menu_ins(under_2_menu, 'Version', 'Check for updates', 'Title')
+
+    column_name = LabelProcess(root, 'Name', 'Tahoma 12', 0, 0)
+    column_type = LabelProcess(root, 'Type', 'Tahoma 12', 0, 1)
+    column_x = LabelProcess(root, 'X', 'Tahoma 12', 0, 2)
+    column_y = LabelProcess(root, 'Y', 'Tahoma 12', 0, 3)
+    def_name = ListProcess(root, 1, 0, ['Figure 1', 'Figure 2', 'Figure 23421', 'Figure 1'])
+    def_name = ListProcess(root, 1, 1, ['    ','','',''])
+    def_name = ListProcess(root, 1, 2, ['    ','','',''])
+    def_name = ListProcess(root, 1, 3, ['    ','','',''])
+    #button_1 = ButtonProcess(root, 'Нет, он - няша', "<Button-1>", 2, 0)
+    #button_1 = ButtonProcess(root, 'Да, пойду налимоню ему очко!', "<Button-1>", 2, 1)
+    #button_1 = ButtonProcess(root, 'Да, пойду налимоню ему очко!', "<Button-1>", 2, 1)
+
+    # root.destroy()
+    root.mainloop()
