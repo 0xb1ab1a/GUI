b373df55718b1834f0f3e4b6d1ede90fd99eefa7
firsh <alex.firsh@gmail.com>
2013-07-30 01:46:13 +0300

    Temp buttons now works perfect, search max in listbox now sexy. Create menu works, but code not ok.
    Buttons now work through "command =", which set action for simple left click, and finally arg name 'new_but' define by ButtonProcess. Optimized search max width in ListProcess. Menu 'Create Oval' and 'rectangle' now works, but code is totally crap. Learn from todo #2.

--- UI.py
+++ UI.py
@@ -3,32 +3,54 @@ from tkinter import *
 
 
 class ButtonProcess():
-    def __init__(self, workspace, text_var, process, row, col):
-        self.but = Button(workspace, text=text_var)
+    def __init__(self, workspace, text_var, new_text, row, col):
+        self.workspace = workspace
+        self.new_text = new_text
+        self.but = Button(workspace, text=text_var, command=self.func_any)
         self.but.grid(row=row, column=col)
-        self.but.bind(process, act.func_any)
+
+
+    def func_any(self):
+        self.new_but = Button(self.workspace, text=self.new_text, command=self.func_any)
+        self.new_but.grid()
+
 
 class Actions():
-    def __init__(self, workspace, text_var, process):
+    def __init__(self, workspace, action):
         self.workspace = workspace
-        self.text_var = text_var
-        self.process = process
+        self.action = action
+
 
     def crt_geom(self):
         """Temp func"""
+
     pass
 
     def destroyer(self):
-        root.destroy()
+        self.workspace.destroy()
 
-    def func_any(self, event):
-        self.new_but = Button(self.workspace, text=self.text_var)
-        self.new_but.pack()
-        self.new_but.bind(self.process, self.func_any)
 
-    def func_new_win(self):
+class New_win():
+    def __init__(self, workspace):
+        self.workspace = workspace
+
+    def main_rect(self):
         self.new_win = Toplevel(self.workspace)
-        self.new_win.title('New window')
+        self.new_win.title('Create')
+        names = ['Name:', 'X:', 'Y:', 'Width:', 'Height:']
+        numb_row = 0
+        for i in names:
+            self.name = LabelProcess(self.new_win, i, 'Tahoma 12', numb_row, 0)
+            numb_row += 1
+
+    def main_oval(self):
+        self.new_win = Toplevel(self.workspace)
+        self.new_win.title('Create')
+        names = ['Name:', 'X:', 'Y:', '[x] Is circle:', 'Radius X:', 'Radius Y: ']
+        numb_row = 0
+        for i in names:
+            self.name = LabelProcess(self.new_win, i, 'Tahoma 12', numb_row, 0)
+            numb_row += 1
 
 
 class LabelProcess():
@@ -39,16 +61,10 @@ class LabelProcess():
 
 class ListProcess():
     def __init__(self, workspace, row, col, name):
-        if len(name) == 1: self.maximum = len(name[0])
-        if len(name) == 0: self.maximum = 2
+        if len(name) == 0:
+            self.maximum = 2
         else:
-            first_it = len(name[0])
-            for j in range(len(name) - 1):
-                if first_it >= len(name[j + 1]):
-                    self.maximum = first_it
-                else:
-                    first_it = len(name[j + 1])
-                    self.maximum = first_it
+            self.maximum = len(max(name, key=len))
         self.listbox = Listbox(workspace, height=len(name), width=self.maximum)
         for i in name:
             self.listbox.insert(END, i)
@@ -75,19 +91,21 @@ class MenuProcess():
         for i, j in inc_dict.items():
             self.menu_internal.add_command(label=i, command=j)
 
+
 if __name__ == "__main__":
     root = Tk()
     root.title('Geometric editor')
-
     base_menu = MenuProcess(root)
-    act = Actions(root, 'Newbut', "<Button-1>")
+    act = Actions(root, "<Button-1>")
+    new_wind = New_win(root)
 
     under_menu = base_menu.menu_col('File', {'Exit': act.destroyer})
     # THINK how pass an argument "workspace" to destroyer
     # under_2_menu = BaseMenu.menu_col('Help')
 
     und_under_menu = base_menu.menu_ins(under_menu, 0, 'Model',
-                                        {'Create rectangle': act.func_new_win, 'Create Oval': act.func_new_win,
+                                        {'Create rectangle': new_wind.main_rect,
+                                         'Create Oval': new_wind.main_oval,
                                          'Remove': act.crt_geom})
 
     # und_under_menu = BaseMenu.menu_ins(under_2_menu, 'Version', 'Check for updates', 'Title')
@@ -97,11 +115,14 @@ if __name__ == "__main__":
     column_x = LabelProcess(root, 'X', 'Tahoma 12', 0, 2)
     column_y = LabelProcess(root, 'Y', 'Tahoma 12', 0, 3)
     def_name = ListProcess(root, 1, 0, ['Figure 1', 'Figure 2', 'Figure 23421', 'Figure 1'])
-    def_name = ListProcess(root, 1, 1, ['    ','','',''])
-    def_name = ListProcess(root, 1, 2, ['    ','','',''])
-    def_name = ListProcess(root, 1, 3, ['    ','','',''])
-    #button_1 = ButtonProcess(root, 'Button name_1', "<Button-1>", 2, 0)
+    def_name = ListProcess(root, 1, 1, ['    ', '', '', ''])
+    def_name = ListProcess(root, 1, 2, ['    ', '', '', ''])
+    def_name = ListProcess(root, 1, 3, ['    ', '', '', ''])
+    #button_1 = ButtonProcess(root, 'Button name_1', "Button-1", 2, 0)
     #button_1 = ButtonProcess(root, 'Button name_2', "<Button-1>", 2, 1)
+
+
+
 
     # root.destroy()
     root.mainloop()
